813f7133b2c9dfef33b08bfe6480345e3a3539d3
Andy Goetz <andy@andygoetz.org>
2014-04-05 12:24:29 -0700

    added comments to script

--- search.py
+++ search.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-# This is a simple cgi script to search for data sheets using google.
+# This is a simple CGI script that simplifies searching for datasheets.
 #
+# See the README for more details
 #
+# GPLv2 license
 
 import json
 import urllib2
@@ -10,9 +12,7 @@ import urllib
 import cgi
 
 
-    
-
-
+# Generates the basic webpage if no query is supplied. Provides a simple search box
 def page_no_query():
     print "Content-Type: text/html"     # HTML is following
     print
@@ -27,17 +27,27 @@ def page_no_query():
 '''
     return
 
+
+# performs a google search for the requested search term, and
+# redirects to the first page that provides a pdf, or to a google
+# search page if the first page of results doesn't have any pdfs.
 def page_query(query):
     print "Status: 302 Found"     # start redirect
+    # the api query
     google_query = 'http://ajax.googleapis.com/ajax/services/search/web?q={0}&v=1.0'.format(query)
+    # the search result webpage if we don't find any results
     failed_query = 'http://google.com/search?q={0}'.format(query)
+    # perform the api query
     response = urllib2.urlopen(google_query)
     results = json.loads(response.read())
+    # search through the json for a pdf page
     for result in results['responseData']['results']:
         if 'fileFormat' in result and result['fileFormat'] == 'PDF/Adobe Acrobat':
+            # if we found it, print that pdf as the destination
             print "Location: {0}".format(result['url'])
             print
             return
+    # Else, print the google search page as the source
     print 'Location: {0}'.format(failed_query)
     print
 
